c415221c6dc5b56c9ee14438b3981ab1c5b62458
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-04-01 20:06:27 +0200

    1.4. kysely 5 lisätty

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ async def get_recipes_daily_by_month(dw: DW, year: int):
                  "FROM rental_transaction_fact " \
                  "INNER JOIN date_dim ON date_dim.date_key = rental_transaction_fact.date_dim_date_key " \
                  "WHERE date_dim.year = :year " \
-                 "GROUP BY date_dim.month ORDER " \
-                 "BY date_dim.month ASC"
+                 "GROUP BY date_dim.month " \
+                 "ORDER BY date_dim.month ASC"
 
     _query = text(_query_str)
 
@@ -103,7 +103,22 @@ async def get_recipes_daily_by_month(dw: DW):
 # 5. Top 10. lainatut tavarat valitulta vuodelta kuukausittain
 
 
+@app.get('/api/recipes/top-ten-items-by-year/{year}')
+async def get_recipes_daily_by_month(dw: DW, year: int):
+    _query_str = "SELECT rental_item_dim.renta_item_name, COUNT(rental_item_dim.renta_item_name) AS esiintymiskerrat " \
+                 "FROM rental_item_fact " \
+                 "INNER JOIN rental_item_dim ON rental_item_dim.item_key = rental_item_fact.rental_item_key " \
+                 "INNER JOIN date_dim ON date_dim.date_key = rental_item_fact.rental_item_creation_date_key " \
+                 "WHERE date_dim.year = 2004 GROUP BY date_dim.month " \
+                 "ORDER BY date_dim.month DESC " \
+                 "LIMIT 10"
 
+    _query = text(_query_str)
 
+    rows = dw.execute(_query, {'year': year})
+
+    data = rows.mappings().all()
+
+    return {'data': data}
 
 # 6. Selvitä missä kuussa tavaroita lisätään järjestelmään eniten valittuna vuonna
