6d7b85f2c28ebf8fe2bb7dc56643508c65f4f13f
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-04-02 14:58:50 +0200

    2.4. kysely 6 lisätty

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def hello_world():
 
 # 1. Lainauksien määrä valitulta kuukaudelta viikottain
 
-@app.get('/api/recipes/weekly-by-month/{month}/{year}')
+@app.get('/api/rental/weekly-by-month/{month}/{year}')
 async def get_recipes_weekly_by_month(dw: DW, month: int, year: int):
     _query_str = "SELECT date_dim.week, COUNT(*) AS transaction_count " \
                  "FROM rental_transaction_fact " \
@@ -40,7 +40,7 @@ async def get_recipes_weekly_by_month(dw: DW, month: int, year: int):
 
 # 2. Lainauksien märää valitulta kuukaudelta päivittäin
 
-@app.get('/api/recipes/daily-by-month/{month}/{year}')
+@app.get('/api/rental/daily-by-month/{month}/{year}')
 async def get_recipes_daily_by_month(dw: DW, month: int, year: int):
     _query_str = "SELECT date_dim.day, COUNT(*) AS transaction_count " \
                  "FROM rental_transaction_fact " \
@@ -59,7 +59,7 @@ async def get_recipes_daily_by_month(dw: DW, month: int, year: int):
 
 # 3. Lainauksien määrä valitulta vuodelta kuukausittain
 
-@app.get('/api/recipes/monthly-by-year/{year}')
+@app.get('/api/rental/monthly-by-year/{year}')
 async def get_recipes_daily_by_month(dw: DW, year: int):
     _query_str = "SELECT date_dim.month, COUNT(*) AS transaction_count " \
                  "FROM rental_transaction_fact " \
@@ -81,7 +81,7 @@ async def get_recipes_daily_by_month(dw: DW, year: int):
 # 4. Kaikkien aikojen top 10. lainatuimmat tavarat
 
 
-@app.get('/api/recipes/top-ten-items')
+@app.get('/api/rental/top-ten-items')
 async def get_recipes_daily_by_month(dw: DW):
     _query_str = "SELECT rental_item_dim.renta_item_name, COUNT(rental_item_dim.renta_item_name) AS esiintymiskerrat " \
                  "FROM rental_item_fact " \
@@ -103,13 +103,14 @@ async def get_recipes_daily_by_month(dw: DW):
 # 5. Top 10. lainatut tavarat valitulta vuodelta kuukausittain
 
 
-@app.get('/api/recipes/top-ten-items-by-year/{year}')
+@app.get('/api/rental/top-ten-items-by-year/{year}')
 async def get_recipes_daily_by_month(dw: DW, year: int):
     _query_str = "SELECT rental_item_dim.renta_item_name, COUNT(rental_item_dim.renta_item_name) AS esiintymiskerrat " \
                  "FROM rental_item_fact " \
                  "INNER JOIN rental_item_dim ON rental_item_dim.item_key = rental_item_fact.rental_item_key " \
                  "INNER JOIN date_dim ON date_dim.date_key = rental_item_fact.rental_item_creation_date_key " \
-                 "WHERE date_dim.year = 2004 GROUP BY date_dim.month " \
+                 "WHERE date_dim.year = 2004 " \
+                 "GROUP BY date_dim.month " \
                  "ORDER BY date_dim.month DESC " \
                  "LIMIT 10"
 
@@ -122,3 +123,27 @@ async def get_recipes_daily_by_month(dw: DW, year: int):
     return {'data': data}
 
 # 6. Selvitä missä kuussa tavaroita lisätään järjestelmään eniten valittuna vuonna
+
+
+# SELECT date_dim.month AS kuukausi, COUNT(rental_item_fact.rental_item_creation_date_key) AS tavaramaara FROM rental_item_fact INNER JOIN rental_item_dim ON rental_item_dim.item_key = rental_item_fact.rental_item_key INNER JOIN date_dim ON date_dim.date_key = rental_item_fact.rental_item_creation_date_key WHERE date_dim.year = 2002 GROUP BY kuukausi ORDER BY tavaramaara DESC LIMIT 1;
+
+
+
+@app.get('/api/rental/top-month-when-adding-items-by-year/{year}')
+async def get_recipes_daily_by_month(dw: DW, year: int):
+    _query_str = "SELECT date_dim.month AS kuukausi, COUNT(rental_item_fact.rental_item_creation_date_key) AS tavaramaara " \
+                 "FROM rental_item_fact " \
+                 "INNER JOIN rental_item_dim ON rental_item_dim.item_key = rental_item_fact.rental_item_key " \
+                 "INNER JOIN date_dim ON date_dim.date_key = rental_item_fact.rental_item_creation_date_key " \
+                 "WHERE date_dim.year = :year " \
+                 "GROUP BY kuukausi " \
+                 "ORDER BY tavaramaara DESC " \
+                 "LIMIT 1"
+
+    _query = text(_query_str)
+
+    rows = dw.execute(_query, {'year': year})
+
+    data = rows.mappings().all()
+
+    return {'data': data}
